5b24955507cae58fd06ad928b82bacb3a2ca2d77
Mariana Barnett <marianabarnett3@gmail.com>
2020-04-15 00:57:15 -0400

    javascript

---
+++ index.js
@@ -0,0 +1,62 @@
+var questions = [
+    {
+        q: "Which country is the origin of the Stella beer?",
+        a: "Belgium",
+        answers:["Belgium", "Brazil"]
+    },
+    {
+        q: "What is the most famous beer in Ireland?",
+        a: "Guiness",
+        answers: ["Guiness", "Beer"]
+        
+    },
+    {
+        q: "What is called a meal in open air?",
+        a: "Picnic",
+        answers: ["Picnic", "Cold meal"]
+    },
+
+    {
+        q: "What is the chemical formula for Table Salt?",
+        a: "NaCl",
+        answers: {
+            a: "true",
+            b: "false"
+        }
+    },
+
+    {
+        q: "What is the most famous Mexican beer?",
+        a: "Corona",
+        answers: {
+            a: "true",
+            b: "false"
+        }
+    },
+
+    {
+        q: "What is the name of the gas that let rise bread dough?",
+        a: "Carbon dioxide",
+        answers: {
+            a: "true",
+            b: "false"
+        }
+    },
+
+]
+
+
+var questions = {
+    question1 = "What is your favorite color?",
+    question2: "How old is your pet?",
+    question3: "Do you like cats?",
+
+};
+
+
+
+
+   
+
+  
+
